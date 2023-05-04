2d800edca70a641ac4a61704ff4ced7a98360c88
Rixtinator <129871232+Rixtinator@users.noreply.github.com>
2023-05-04 18:00:43 +0200

    make the single round into a real function

--- game.js
+++ game.js
@@ -27,14 +27,18 @@ playerSelection = playerSelection.toUpperCase();
 console.log(playerSelection);
 
 /* A function that plays a single round of Rock Paper Scissors." */
-let gameResult = "";
-gameResult = (playerSelection === computerChoice) ? "You choose the same, try again."
-    : (playerSelection === "ROCK" && computerChoice === "PAPER") ? "You lose! Paper beats Rock!"
-        : (playerSelection === "ROCK" && computerChoice) === "SCISSOR" ? "You win! Rock beats Scissor!"
-            : (playerSelection === "PAPER" && computerChoice === "ROCK") ? "You win! Paper beats Rock!"
-                : (playerSelection === "PAPER" && computerChoice === "SCISSOR") ? "You lose! Scissor beats Paper!"
-                    : (playerSelection === "SCISSOR" && computerChoice === "PAPER") ? "You win! Scissor beats Paper!"
-                        : (playerSelection === "SCISSOR" && computerChoice === "ROCK") ? "You lose! Rock beats Scissor!"
-                            : "Oops, something went wrong."
 
-console.log(gameResult);
+let gameResult = "";
+
+function playRound() {
+    return (playerSelection === computerChoice) ? gameResult = "You choose the same, try again."
+        : (playerSelection === "ROCK" && computerChoice === "PAPER") ? gameResult = "You lose! Paper beats Rock!"
+            : (playerSelection === "ROCK" && computerChoice) === "SCISSOR" ? gameResult = "You win! Rock beats Scissor!"
+                : (playerSelection === "PAPER" && computerChoice === "ROCK") ? gameResult = "You win! Paper beats Rock!"
+                    : (playerSelection === "PAPER" && computerChoice === "SCISSOR") ? gameResult = "You lose! Scissor beats Paper!"
+                        : (playerSelection === "SCISSOR" && computerChoice === "PAPER") ? gameResult = "You win! Scissor beats Paper!"
+                            : (playerSelection === "SCISSOR" && computerChoice === "ROCK") ? gameResult = "You lose! Rock beats Scissor!"
+                                : "Oops, something went wrong."
+}
+
+console.log(playRound(gameResult));
