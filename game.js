759e137d804a5ede7381a93258d4d004b31dd913
Rixtinator <129871232+Rixtinator@users.noreply.github.com>
2023-05-04 11:41:23 +0200

    get random number of 1, 2 or 3 function

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-console.log("Hello World"); 
+/*a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.*/
+
+/* 
+make a variable called getComputerChoice
+make a random function to get random output
+when 1 return rock
+when 2 return paper 
+when 3 return scissors
+*/
+
+let randomNumber = 0;
+
+function getRandomNumber() {
+    return randomNumber = Math.floor(Math.random() * 3) + 1;
+}
+
+getRandomNumber();
+
+/*function getComputerChoice() {
+}*/
